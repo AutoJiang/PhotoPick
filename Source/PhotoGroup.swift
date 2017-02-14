e80a0561078d7c41dc53df5f4c79ea4e159b34d1
jiangaoteng.auto <675917545@qq.com>
2017-02-14 14:46:39 +0800

    添加PhotoGroup

--- Source/PhotoGroup.swift
+++ Source/PhotoGroup.swift
@@ -10,10 +10,20 @@ import Foundation
 import AssetsLibrary
 
 struct PhotoGroup {
+    
     var assetModel: [PhotoModel]
     var assetGroup: ALAssetsGroup
+    var name: String = ""
     
     static func findAllGroups(callback: ([PhotoGroup]) -> Void){
+        
+    }
+    
+    static func findAllPhotoModels(callback: ([PhotoModel]) -> Void){
+        
+    }
+    
+    static func findAllPhotoModels(by group: PhotoGroup, callback: ([PhotoModel]) -> Void){
         
     }
 
