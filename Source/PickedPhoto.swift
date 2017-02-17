324c8d184675f3207d0ce4e1609d7c187b42994f
jiangaoteng.auto <675917545@qq.com>
2017-02-17 18:57:09 +0800

    重构

--- Source/PickedPhoto.swift
+++ Source/PickedPhoto.swift
@@ -10,13 +10,18 @@ import UIKit
 import AssetsLibrary
 
 public class PickedPhoto: NSObject {
+    enum PhotoType {
+        case asset(asset: ALAsset)
+        case image(image: UIImage)
+    }
+    
     private var asset : ALAsset?
     private var privateImage: UIImage?
     private let maxSidePixels : CGFloat = 1280
     private let minStretchSidePixels : CGFloat = 440
     
-    //原图
-    public var image: UIImage {
+    ///原图
+    public var originalImage: UIImage {
         get{
             if let privateImage = privateImage {
                 return privateImage
@@ -25,11 +30,22 @@ public class PickedPhoto: NSObject {
             return AssetTool.imageFromAsset(representation: asset!.defaultRepresentation())
         }
     }
-    //返回压缩图 compress
-    public var compressedImage:UIImage{
+    
+    ///返回压缩图 compress
+    public var image: UIImage {
         get{
-            return self.image.gkit_scale(toMaxSidePixels: maxSidePixels, minStretchSidePixels: minStretchSidePixels)
+            return self.originalImage.gkit_scale(toMaxSidePixels: maxSidePixels, minStretchSidePixels: minStretchSidePixels)
         }
+    }
+    
+    /// TODO
+    public var isGIF: Bool {
+        return false
+    }
+    
+    ///创建一个图片文件路径，创建后由外部自行删除
+    public func newAnImageFile() -> String {
+        return ""
     }
     
     init(asset : ALAsset) {
